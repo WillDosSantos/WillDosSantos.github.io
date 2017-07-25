15cf0f479e58bebeae4b52432d85d590b81d9a04
William <will@ministrycentered.com>
2017-07-25 10:16:32 -0700

    work on pop up

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,7 @@
-$ = function(id) {
-  return document.getElementById(id);
-}
-
-var show = function(id) {
-	$(id).style.display ='block';
-}
-var hide = function(id) {
-	$(id).style.display ='none';
-}
+(function($) {
+  $(function() {
+    $('.toggle-overlay').click(function() {
+      $('.overlay-container').toggleClass('open');
+    });
+  });
+})(jQuery);
